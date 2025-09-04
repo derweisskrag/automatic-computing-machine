e7e1a8e069262f34b2400a901ddd4127dcd53ca8
Sergei Ivanov <kompasj@mail.ru>
2025-09-04 11:10:29 +0300

    Remove tomodachi_labs, will use submodule

--- tomodachi_labs/python_labs/print_one_billion/test_count_to_billion.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from count_to_billion import count_to_billion
-
-
-def test_count_to_billion(benchmark):
-    benchmark(count_to_billion)
-    
-
--- tomodachi_labs/rust_labs/count_billion/src/main.rs
+++
@@ -1,6 +0,0 @@
-use count_billion::{count_to_billion};
-
-fn main() {
-    let result = count_to_billion();
-    println!("Got the result: {}", result);
-}
